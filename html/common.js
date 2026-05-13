3b530da55ae4a6c4877a7e10fc7f26532db99c40
Sushravya40 <sushravya1723@gmail.com>
2026-05-13 13:20:56 +0530

    Update login flow to dynamically display username

--- html/common.js
+++ html/common.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const signInBtn = document.getElementById('sign');
     
     if (signInBtn) {
-        // If user is logged in, change button to Profile
+        // If user is logged in, change button to their username
         if (localStorage.getItem('isLoggedIn') === 'true') {
-            signInBtn.innerText = 'Profile';
+            const uname = localStorage.getItem('username') || 'Profile';
+            signInBtn.innerText = uname;
             signInBtn.href = 'profile.html';
         }
     }
